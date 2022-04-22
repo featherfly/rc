a704a2f4648a9e049d2e970f7d8b39957829d068
zhongji <featherfly@foxmail.com>
2022-04-22 14:36:27 +0800

    chore: 加入sql文件

---
+++ configuration-sqldb/src/main/resources/META-INF/rc/mysql/rc-configuration-init.sql
@@ -0,0 +1,23 @@
+-- ----------------------------
+-- Table structure for rc_configuration_difinition
+-- ----------------------------
+DROP TABLE IF EXISTS `rc_configuration_difinition`;
+CREATE TABLE `rc_configuration_difinition`  (
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `descp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`NAME`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for rc_configuration_value
+-- ----------------------------
+DROP TABLE IF EXISTS `rc_configuration_value`;
+CREATE TABLE `rc_configuration_value`  (
+  `config_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
+  `descp` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `value` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`config_name`, `name`) USING BTREE,
+  CONSTRAINT `FK_REFERENCE_CONFIG_NAME` FOREIGN KEY (`name`) REFERENCES `rc_configuration_difinition` (`name`) ON DELETE
+   RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
